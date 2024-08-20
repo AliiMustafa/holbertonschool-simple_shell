70841da0925c406f49eba35a5d9e2a4b6e982bc2
Elyaaa001 <gabibovelina75@gmail.com>
2024-08-20 15:21:33 +0400

    code error

--- main.c
+++ main.c
@@ -1,67 +1,61 @@
 #include "main.h"
-extern char **environ;
-
 int main(void)
 {
-    char *buf = NULL, *token;
-    size_t count = 0;
-    ssize_t nread;
-    pid_t child;
-    char *commands[100];
-    int i, status;
-    int f = 1;
+	char *buf, *token;
+	size_t count = 0;
+	ssize_t nread;
+	pid_t child;
+	char *commands[100];
+	int i, status, f = 1;
 
-    while (f)
-    {
-        printf("$ ");
+	while (f)
+	{
+		buf = NULL;
 
-        nread = getline(&buf, &count, stdin);
-        if (nread == -1)
-        {
-            free(buf);
-            exit(EXIT_SUCCESS);
-        }
+		nread = getline(&buf, &count, stdin);
 
-        // Remove trailing newline if present
-        buf[strcspn(buf, "\n")] = '\0';
-
-        token = strtok(buf, " \n");
-        i = 0;
-        while (token)
-        {
-            commands[i] = token;
-            token = strtok(NULL, " \n");
-            i++;
-        }
-        commands[i] = NULL;
-
-        child = fork();
-        if (child == -1)
-        {
-            perror("Fork failed");
-            free(buf);
-            exit(EXIT_FAILURE);
-        }
-        else if (child == 0)
-        {
-            if (execve(commands[0], commands, environ) == -1)
-            {
-                perror("Failed to execute");
-                free(buf);
-                exit(EXIT_FAILURE);
-            }
-        }
-        else
-        {
-            if (waitpid(child, &status, 0) == -1)
-            {
-                perror("Wait failed");
-                free(buf);
-                exit(EXIT_FAILURE);
-            }
-        }
-    }
-
-    free(buf);
-    return 0;
+		if (nread ==  -1)
+		{
+			f = 0;
+			free(buf);
+                        exit(0);
+                }
+                token = strtok(buf, " \n");
+		i = 0;
+                while (token)
+                {
+                        commands[i] = token;
+                        token = strtok(NULL, " \n");
+                        i++;
+                }
+                commands[i] = NULL;
+                child = fork();
+                if (child == -1)
+                {
+			free(buf);
+                        perror("Fork failed");
+			exit(EXIT_FAILURE);
+                }
+		else if (child == 0)
+                {
+                        if (execve(commands[0], commands, NULL) == -1)
+                        {
+				free(buf);
+                                perror("Failed to execute");
+                                exit(97);
+                        }
+		}
+		else
+		{
+			if (wait(&status) == -1)
+			{
+				free(buf);
+				perror("Wait failed");
+				exit(EXIT_FAILURE);
+			}
+		}
+	}
+        free(buf);
+        return (0);
 }
+
