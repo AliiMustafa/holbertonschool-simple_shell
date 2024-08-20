b982691e91f6724c1646ad60596ee4eb101c107d
AliiMustafa <aliimustafazada@gmail.com>
2024-08-20 17:41:26 +0400

    Change the error message

--- main.c
+++ main.c
@@ -31,7 +31,7 @@ int main(void)
 		else if (child == 0)
 		{
 			if (execve(commands[0], commands, NULL) == -1)
-				free(buf), fprintf(stderr, "./hsh: 1: %s: not found\n", commands[0]), exit(EXIT_FAILURE);
+				free(buf), exit(EXIT_FAILURE);
 		}
 		else
 		{
