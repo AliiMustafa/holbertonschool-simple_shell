bc7779a44b6b11668b1f5090ff38f1f3c16927c4
AliiMustafa <aliimustafazada@gmail.com>
2024-08-21 14:36:25 +0400

    Add new new file trim and change in main.c file

--- main.c
+++ main.c
@@ -5,41 +5,54 @@
  */
 int main(void)
 {
-	char *buf, *token, *commands[100];
-	size_t count = 0;
-	ssize_t nread;
+	char *buf, *token, *commands[100], *trimmed;
+	size_t count, i;
 	pid_t child;
-	int i, status, f = 1;
+	int status;
 
-	while (f)
+	while (1)
 	{
-		buf = NULL;
-		nread = getline(&buf, &count, stdin);
-		if (nread ==  -1)
-			f = 0, free(buf), exit(0);
+		i = 0, buf = NULL;
+		if (getline(&buf, &count, stdin) == -1)
+			free(buf), exit(0);
+		trimmed = trim(buf);
+		if (trimmed[0] == '\n')
+		{
+			free(trimmed), free(buf);
+			continue;
+		}
+		free(trimmed);
 		token = strtok(buf, " \n");
-		i = 0;
-		while (token)
+		while(token)
 		{
 			commands[i] = token;
 			token = strtok(NULL, " \n");
 			i++;
 		}
-		commands[i] = NULL, child = fork();
+		commands[i] = NULL;
+		if (strcmp(commands[i - 1], "exit") == 0)
+		{
+			free(buf);
+			if (i == 1)
+				exit(0);
+			else
+				exit(2);
+		}
+		child = fork();
 		if (child == -1)
 			free(buf), perror("Fork failed"), exit(EXIT_FAILURE);
 		else if (child == 0)
 		{
 			if (execve(commands[0], commands, NULL) == -1)
-				free(buf), exit(EXIT_FAILURE);
+				free(buf), perror("we found error"), exit(0);
 		}
 		else
 		{
 			if (wait(&status) == -1)
-				free(buf), perror("Wait failed"), exit(EXIT_FAILURE);
+				free(buf), perror("wait has failed"), exit(0);
 		}
 		free(buf);
 	}
+
 	return (0);
 }
-
