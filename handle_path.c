c2ffbba792b34425274ce3f38d4629b9c2f50a1d
root <aliimustafazada@gmail.com>
2024-08-22 23:27:29 +0000

    deallocate path

--- handle_path.c
+++ handle_path.c
@@ -48,6 +48,7 @@ char *handle_path(char *command)
 	{
 		fprintf(stderr, "./hsh: 1: %s: not found\n", command);
 		free(command1);
+		free(direc);
 		exit(127);
 	}
 	d = malloc(sizeof(char) * (strlen(direc) + 1));
